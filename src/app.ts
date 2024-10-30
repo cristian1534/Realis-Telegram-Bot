c41d03dd31408fca318b6f6af6778f1abf557c9b
Development <development@Developments-MacBook-Pro.local>
2024-10-30 02:36:26 +0200

    Monitoring Prometheus - Grafana

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,25 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import client from "prom-client"; 
 
 import messagesRouter from "./infrastructure/routes/messages";
 
 const app = express();
 const PORT = process.env.PORT || 4040;
 
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics(); 
+
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
 app.use(express.json());
 app.use(cors());
-
 app.use("/", messagesRouter);
 
 app.listen(PORT, () => {
